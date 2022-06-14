ad06f5cea4bf4a89508a9490357e5e4312aa16ff
MurabitoF <francomurabito@outlook.com>
2022-06-13 22:54:18 -0300

    init project

---
+++ src/main/java/com/example/tplav5/TpLav5Application.java
@@ -0,0 +1,13 @@
+package com.example.tplav5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpLav5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpLav5Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tplav5/TpLav5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tplav5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpLav5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
